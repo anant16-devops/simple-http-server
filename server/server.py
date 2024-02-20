0b908bbe1499f0791aa4529f7c023c4becda631d
Jasmeet <js3393187@gmail.com>
2024-02-20 11:46:51 +0530

    added buffer for incoming req, directory listing, serve file func improved, refactoring

--- server/server.py
+++ server/server.py
@@ -1,36 +1,40 @@
 import socket
-import sys
-from myutils import get_status_texts, parse_request
+import logging
+from routes import routes
+import os
+from .utils import (
+    get_allowed_headers,
+    get_status_texts,
+    commandline_parser,
+    get_mime_type,
+    get_content_length,
+    is_binary_mime_type,
+    parse_request,
+    create_dirlist_page,
+    create_error_page,
+    parse_path,
+)
+
+ROOT_PATH = "./static/"
+
+
+def setup_logger():
+    pass
 
 
 def start_server():
-    host = "127.0.0.1"
-    port = 8080
-    if len(sys.argv) == 1:
-        pass
-    elif len(sys.argv[1:]) % 2 == 0:
-        for i in range(1, len(sys.argv), 2):
-            if sys.argv[i] == "-h":
-                host = sys.argv[i + 1]
-            elif sys.argv[i] == "-p":
-                port = int(sys.argv[i + 1])
-            else:
-                print(f"Unsupported arguement: {sys.argv[i]}")
-    else:
-        print("Usage: python script.py [-h <host>] [-p <port>]")
-        return
+    host, port, directory = commandline_parser()
 
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind((host, port))
-
         sock.listen()
         print(f"Listening on {host}:{port}")
 
         while True:
             c_socket, c_address = sock.accept()
             print(f"Connected to {c_address}")
-            handle_request(c_socket)
+            handle_request(c_socket, directory)
     except KeyboardInterrupt:
         print("Server terminated by user")
     # except Exception as e:
@@ -40,82 +44,185 @@ def start_server():
 
 
 def send_http_response(
-    client_socket, response_body, status_code, status_message, content_type="text/plain"
+    client_socket,
+    response_body,
+    status_code,
+    status_message,
+    content_type="text/plain",
+    encoding="utf-8",
 ):
     try:
-        response = f"HTTP/1.1 {status_code} {status_message}\r\nContent-Length: {len(response_body)}\r\nContent-Type:{content_type}\r\n\r\n{response_body}"
-        client_socket.sendall(response.encode("utf-8"))
-    except BrokenPipeError:
-        print("Client Disconnected")
+        if is_binary_mime_type(content_type):
+            response_header = f"HTTP/1.1 {status_code} {status_message}\r\nContent-Length: {len(response_body)}\r\nContent-Type: {content_type}\r\n\r\n"
+            client_socket.sendall(response_header.encode(encoding))
+            client_socket.sendall(response_body)
+        else:
+            response = f"HTTP/1.1 {status_code} {status_message}\r\nContent-Length: {len(response_body)}\r\nContent-Type: {content_type}\r\n\r\n{response_body}"
+            client_socket.sendall(response.encode(encoding))
+
+    except BrokenPipeError as pipe_error:
+        alt_message = "Connection has been Terminated"
+        if hasattr(pipe_error, "args") and pipe_error.args:
+            alt_message += f"\nAdditional Details: {', '.join(str(args) for args in pipe_error.args)}"
+        print(alt_message)
 
 
 def handle_unsupported_request(client_socket):
-    response_data = "Not Implemented: Unsupported HTTP method"
-    send_http_response(client_socket, response_data, 501, get_status_texts(501))
+    err_page = create_error_page(501)
+    send_http_response(client_socket, err_page, 501, get_status_texts(501), "text/html")
 
 
-def handle_request(client_socket):
-    with client_socket:
-        data = client_socket.recv(1024)
-        request_data = parse_request(data)
-        if len(request_data) == 0:
-            serve_error_page(client_socket, 500)
-            # send_http_response(
-            #     client_socket, get_status_texts(500), 500, get_status_texts(500)
-            # )
-        print(request_data)
-
-        if request_data.get("method") != "GET":
-            handle_unsupported_request(client_socket)
-        else:
-            handle_get_request(
-                client_socket, request_data.get("path"), request_data.get("metadata")
-            )
-
-
-def handle_get_request(client_socket, path, metadata):
-    print(f"\nMetadata dictionary: {metadata}\n")
-    if (path == "/") and (
-        "*/*" in metadata.get("accept")
-        or "text/plain" in metadata.get("accept")
-        or "text/html" in metadata.get("accept")
-    ):
-        serve_file(client_socket, "index.html")
-    elif path == "/ping":
-        serve_file(client_socket, "pong.html")
-    else:
-        serve_error_page(client_socket, 404)
-
-
-def serve_file(client_socket, file_name):
-    default_path = f"./static/{file_name}"
-    if "/" in file_name or "\\" in file_name:
-        default_path = file_name
-
+def handle_request(client_socket: socket.socket, directory=None):
+    buffer = b""
+    client_socket.settimeout(10)
     try:
-        with open(default_path, "r") as f:
-            data = f.read()
+        body_buffer = None
+        with client_socket:
+            while True:
+                data = client_socket.recv(1024)
+                if not data:
+                    break
+                buffer += data
+
+                if b"\r\n\r\n" in buffer:
+                    header_index = buffer.find(b"\r\n\r\n") + len(b"\r\n\r\n")
+                    header_buffer = buffer[:header_index]
+                    body_buffer = buffer[header_index:]
+
+                    has_contentlength = get_content_length(header_buffer)
+                    if has_contentlength:
+                        while len(body_buffer) < has_contentlength:
+                            body_data = client_socket.recv(1024)
+                            if not body_data:
+                                break
+                            body_buffer += body_data
+                            buffer += body_buffer
+                    break
+
+            print(buffer)
+            print("body buffer: ", body_buffer)
+            # parsed_header = parse_header(header_buffer)
+
+            # parsed_body = parse_body(body_buffer)
+
+            request_data = parse_request(buffer)
+            if len(request_data) == 0:
+                send_http_response(
+                    client_socket,
+                    "Incorrect http request format",
+                    400,
+                    get_status_texts(400),
+                )
+            print(request_data)
+
+            if request_data["headers"].get("method") != "GET":
+                handle_unsupported_request(client_socket)
+            else:
+                handle_get_request(
+                    client_socket, request_data.get("headers"), directory
+                )
+
+    except KeyError:
+        print("Failed to Parse request")
+
+    except socket.timeout:
+        print("Connection Timeout")
+
+
+def handle_get_request(client_socket, getreq_data, directory):
+    print(f"\nMetadata dictionary: {getreq_data.get('metadata')}\n")
+    path = getreq_data.get("path")
+    if directory:
+        handle_directory_listing(client_socket, directory, path)
+    else:
+        allowed_headers = get_allowed_headers()
+        accept_headers = getreq_data.get("metadata").get("accept")
+        request_mime_type = get_mime_type(path)
+        if any(header in accept_headers for header in allowed_headers):
+            for route in routes:
+                if route.get(path, ""):
+                    serve_file(client_socket, route[path])
+            else:
+                serve_error_page(client_socket, 404)
+        else:
             send_http_response(
-                client_socket, data, 200, get_status_texts(200), "text/html"
+                client_socket, "415 Unsupported Media Type", 415, get_status_texts(415)
             )
-    except FileNotFoundError:
+
+
+def handle_directory_listing(client_socket, directory_path, url_path):
+    stripped_urL_path = url_path.lstrip("/")
+    combined_path = os.path.normpath(os.path.join(directory_path, stripped_urL_path))
+    decoded_path = parse_path(combined_path, False)
+    if os.path.isdir(combined_path):
+        page = create_dirlist_page(combined_path, stripped_urL_path)
+        if not page:
+            serve_error_page(client_socket, 404)
+        else:
+            send_http_response(
+                client_socket, page, 200, get_status_texts(200), "text/html"
+            )
+    elif os.path.isfile(decoded_path):
+        serve_file(client_socket, decoded_path, True)
+    else:
+        message = "Directory or file not found"
         serve_error_page(client_socket, 404)
+        print(message)
+
+
+def serve_file(client_socket, file_path: str, directory_serve=False):
+    if not directory_serve:
+        norm_path = os.path.normpath(os.path.join(ROOT_PATH, file_path.lstrip("/")))
+        mime_type = get_mime_type(norm_path)
+        if os.path.isfile(norm_path):
+            try:
+                with open(norm_path, "r") as f:
+                    data = f.read()
+                    send_http_response(
+                        client_socket, data, 200, get_status_texts(200), mime_type
+                    )
+            except FileNotFoundError:
+                print("File does not exist")
+                serve_error_page(client_socket, 404)
+            except UnicodeDecodeError:
+                print(f"UnicodeDecodeError on file {norm_path}")
+                serve_error_page(client_socket, 500)
+            except UnicodeEncodeError:
+                print(f"UnicodeEncodeError on file {norm_path}")
+                serve_error_page(client_socket, 500)
+        else:
+            print("File does not exist")
+            serve_error_page(client_socket, 404)
+    else:
+        norm_path = os.path.normpath(file_path)
+        mime_type = get_mime_type(norm_path)
+        try:
+            if is_binary_mime_type(mime_type):
+                with open(norm_path, "rb") as f:
+                    data = f.read()
+                    send_http_response(
+                        client_socket, data, 200, get_status_texts(200), mime_type
+                    )
+            else:
+                with open(norm_path, "r") as f:
+                    data = f.read()
+                    send_http_response(
+                        client_socket, data, 200, get_status_texts(200), mime_type
+                    )
+        except FileNotFoundError:
+            print("File does not exist")
+            serve_error_page(client_socket, 404)
+        except UnicodeDecodeError:
+            print(f"UnicodeDecodeError on file {norm_path}")
+            serve_error_page(client_socket, 500)
 
 
 def serve_error_page(client_socket, error_code):
-    file_to_serve = f"{error_code}_error_page"
-    try:
-        with open(f"./static/errors/{file_to_serve}.html", "r") as f:
-            data = f.read()
-            send_http_response(
-                client_socket,
-                data,
-                error_code,
-                get_status_texts(error_code),
-                "text/html",
-            )
-    except FileNotFoundError:
-        send_http_response(client_socket, "", 500, get_status_texts(500))
+    err_page = create_error_page(error_code)
+    send_http_response(
+        client_socket, err_page, error_code, get_status_texts(error_code), "text/html"
+    )
 
 
-start_server()
+if __name__ == "__main__":
+    start_server()
