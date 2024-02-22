68b0f1434564a0828255c7e49f23dc9770b8f101
Jasmeet <js3393187@gmail.com>
2024-02-22 16:13:04 +0530

    Shifted routes.py to a module folder

--- routes/routes.py
+++ routes/routes.py
@@ -1,7 +1,7 @@
 from server.utils import route, route_list
 
 routes = route_list(
-    route('/', 'index.html'),
-    route('/about', 'aboutme.html'),
-    route('/ping', 'pong.html')
+    route("/", "index.html"),
+    route("/about", "aboutme.html"),
+    route("/ping", "pong.html"),
 )
