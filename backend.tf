90ccb6711c94626313b2f8b24f355e7e8a0f9dbc
Deora <anant.deora@accenture.com>
2025-07-07 20:24:20 +0530

    Simple Http Server on AWS EC2 instance

---
+++ backend.tf
@@ -0,0 +1,8 @@
+# Uncomment and configure if you want to use remote state
+# terraform {
+#   backend "s3" {
+#     bucket = "your-terraform-state-bucket"
+#     key    = "simple-http-server/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
